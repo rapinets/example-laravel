e96f8dbd42af749e997dba52e444851caaa8082f
Rapynets Vasyl <rapinecvasiliy@gmail.com>
2023-03-27 07:24:10 +0300

    create table posts and make migration

--- database/migrations/2023_03_27_035153_create_posts_table.php
+++ database/migrations/2023_03_27_035153_create_posts_table.php
@@ -11,15 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->string('title');
+            $table->text('content');
+            $table->boolean('active')->default(true);
         });
     }
 
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 };
